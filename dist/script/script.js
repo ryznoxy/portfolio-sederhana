e22925d83792954c967c2baf0fc2e1543adbb77d
rzznoxy <rezajelek2@gmail.com>
2023-08-30 19:57:25 +0700

    portfolio ryznoxy part-1

---
+++ dist/script/script.js
@@ -0,0 +1,78 @@
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+
+hamburger.addEventListener('click', function(){
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+} ) 
+
+
+
+window.onscroll = function(){
+  const header = document.getElementById('header'); 
+  const fixNav = header.offsetTop;
+
+  if(window.pageYOffset > fixNav){
+    header.classList.add('navbar-blur');
+  }else{
+    header.classList.remove('navbar-blur');
+  }
+
+}
+
+const text = document.querySelector('.sec-text');
+const textLoad = () => {
+  setTimeout(() => {
+    textLoad.textContent = "Progammer"
+  }, 0);
+
+  setTimeout(() => {
+    textLoad.textContent = "Designer"
+  }, 4000);
+  setTimeout(() => {
+    textLoad.textContent = "Developer"
+  }, 8000);
+}
+textLoad();
+setInterval(textLoad,12000);
+
+
+if ($(".text-slider").length == 1) {
+
+  var typed_strings =
+      $(".text-slider-items").text();
+
+  var typed = new Typed(".text-slider", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 50,
+      loop: true,
+      backDelay: 900,
+      backSpeed: 30,
+  });
+}
+
+new Typed('#typed',{
+  strings : ['Developer','Designer','Progammer',],
+  typeSpeed: 100,
+  backSpeed: 30,
+  delaySpeed : 1000,
+  loop : true
+});
+
+
+
+
+
+/*window.onscroll = function(){
+  const nav = document.getElementById('right-nav');
+  const fixNav = nav.offsetTop;
+
+  if(window.pageYOffset > fixNav){
+    nav.classList.remove('hidden');
+  }else{
+    nav.classList.add('hidden');
+  }
+
+}*/
+
