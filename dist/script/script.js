e53b92ced33d88b0f520eb8f371fb49743770a06
rzznoxy <rezajelek2@gmail.com>
2023-08-31 17:47:40 +0700

    Add Timeline

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,13 +1,19 @@
+//hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
-
 
 hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 } ) 
 
-
+//klik diluar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
 
 window.onscroll = function(){
   const header = document.getElementById('header'); 
@@ -52,6 +58,7 @@ if ($(".text-slider").length == 1) {
   });
 }
 
+//typed
 new Typed('#typed',{
   strings : ['Developer','Designer','Progammer',],
   typeSpeed: 100,
